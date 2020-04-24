ce2fba2fc99dd95cad9d3eb3a5dc38f629d49d18
SlipFIl <soliderslipfil@gmail.com>
2020-04-24 15:34:47 +0300

    some ch
    some ch

--- src/components/sider/camList/camlist.jsx
+++ src/components/sider/camList/camlist.jsx
@@ -52,18 +52,18 @@ const grid = 8;
 const getItemStyle = (isDragging, draggableStyle) => ({
   // some basic styles to make the items look a bit nicer
   userSelect: "none",
-  padding: grid * 2,
+  padding: grid / 2 ,
   margin: `0 0 ${grid}px 0`,
 
   // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? "lightgreen" : "white",
 
   // styles we need to apply on draggables
   ...draggableStyle
 });
 
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
+  background: isDraggingOver ? "lightblue" : "white",
   padding: grid,
   width: 250,
   height: 700,
